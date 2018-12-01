c3807d2218923da455044c66e8b33bacc6ce359e
Laimonas Janutėnas <laimonas.janutenas@gmail.com>
2018-12-01 21:05:22 +0200

    square root example

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -1,19 +1,29 @@
 #include <stdio.h>
+#include <iostream>
 
 int main() {
 
+	__declspec(align(16))float fmas[16] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
+	__declspec(align(16))float fgmas[16];
+	int imsize = sizeof(fmas) / sizeof(float);
 
-	__asm {
-		push eax
-		push ebx
-		push ecx
-		push edx
-		//---------------------------------------------------------------------------------------------
+	float * fptr;
+	float * fgptr;
 
-		//---------------------------------------------------------------------------------------------
-		pop eax
-		pop ebx
-		pop ecx
-		pop edx
+	for (int i = 0; i < imsize; i += 4) {
+		fptr = fmas + i;
+		fgptr = fgmas + i;
+			__asm {
+			mov eax, fptr
+			movaps xmm0,[eax]
+			sqrtps xmm0,xmm0
+			mov eax,fgptr
+			movaps [eax],xmm0
+		}
 	}
+	for (int i = 0; i < 16; i++) {
+		std::cout << fmas[i] << " " << fgmas[i] << std::endl;
+	}
+	system("pause");
+
 }
